8b83f458e74a1202047a110d0ca7d845093a7a91
Mim Hufford <Mim Hufford>
2016-12-19 12:13:56 +0000

    day 1 - broke movements up into 1 unit at a time

--- 1.js
+++ 1.js
@@ -3,14 +3,19 @@ const input = 'R2, L3, R2, R4, L2, L1, R2, R4, R1, L4, L5, R5, R5, R2, R2, R1, L
 const move = (turn, amount) => ({x, y, facing}) => {
   const directions = ['N', 'E', 'S', 'W']
   const nowFacing = directions[(directions.indexOf(facing) + (turn == "R" ? 1 : 3)) % 4]
-  const movement = { "N": [0, amount], "E": [amount, 0], "S": [0, -amount], "W": [-amount, 0] }
-  return { x: x + movement[nowFacing][0], y: y + movement[nowFacing][1], facing: nowFacing }
+  const movement = { "N": [0, 1], "E": [1, 0], "S": [0, -1], "W": [-1, 0] }
+
+  return Array(amount - 1).fill().reduce((prev, curr) =>
+    // take the previous moves and add on one more based on one evolution of the latest move
+    [...prev, { x: prev[prev.length - 1].x + movement[nowFacing][0], y: prev[prev.length - 1].y + movement[nowFacing][1], facing: nowFacing }],
+    // start with one movement done
+    [{ x: x + movement[nowFacing][0], y: y + movement[nowFacing][1], facing: nowFacing }])
 }
 
-const finalPosition = input.trim().split(",")
+const allPositions = input.trim().split(",")
   .map(s => s.trim())
   .map(s => [s.slice(0, 1), parseInt(s.slice(1))])
   .map(i => move(i[0], i[1]))
-  .reduce((data, moveFn) => moveFn(data), { x: 0, y: 0, facing: "N" })
+  .reduce((data, moveFn) => [...data, ...moveFn(data[data.length - 1])], [{ x: 0, y: 0, facing: "N" }])
 
-console.log(finalPosition)
+console.log(allPositions.slice(-1))
