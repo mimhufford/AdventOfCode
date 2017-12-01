f908a7de7b1983134bbfd61c76375a9725d00393
Mim Hufford <mim@hotmail.co.uk>
2017-12-01 06:18:48 +0000

    day 1 part 2

--- 2017/1.js
+++ 2017/1.js
@@ -1,6 +1,11 @@
 const input = require("./data").day1
-const _ = require('ramda')
 
-const data = _.aperture(2, input + input[0])
-const result = data.map(pair => pair[0] == pair[1] ? parseInt(pair[0]) : 0).reduce((sum, next) => sum + next)
-console.log(result);
+const data1 = input.split('').map((v, i) => [v, input[(i + 1) % input.length]])
+const result1 = data1.map(pair => pair[0] == pair[1] ? parseInt(pair[0]) : 0).reduce((sum, next) => sum + next)
+
+const half = input.length / 2;
+const data2 = input.split('').map((v, i) => [v, input[(i + half) % input.length]])
+const result2 = data2.map(pair => pair[0] == pair[1] ? parseInt(pair[0]) : 0).reduce((sum, next) => sum + next)
+
+console.log(result1)
+console.log(result2)
