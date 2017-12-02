b3f20e5a29918de21c6db848ee90605a318a7048
Mim Hufford <mim@hotmail.co.uk>
2017-12-02 10:41:20 +0000

    day 2 part 2 - absolutely horrible code

--- 2017/2.js
+++ 2017/2.js
@@ -5,6 +5,16 @@ const min = (a, b) => a < b ? a : b
 const max = (a, b) => a > b ? a : b
 
 const data = input.split('\n').map(line => line.split('\t').map(Number))
-const sum = data.map(row => row.reduce(max) - row.reduce(min)).reduce(add)
+const part1 = data.map(row => row.reduce(max) - row.reduce(min)).reduce(add)
+const part2 = data
+                .map(row => row
+                    .map((v1, i1) => row
+                        .map((v2, i2) => v1 > v2 ? v1 % v2 == 0 ? v1 / v2 : 0 : 0)
+                        .filter(v => v != 0))
+                    .filter(a => a.length != 0))
+                .map(v => v[0])
+                .map(v => v[0])
+                .reduce(add)
 
-console.log(sum)
+console.log(part1);
+console.log(part2)
