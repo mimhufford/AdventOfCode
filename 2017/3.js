7b9142d7f301ba11098d5b7705e49d0842a852e1
Mim Hufford <mim@hotmail.co.uk>
2017-12-04 10:29:43 +0000

    day 3 part 2 - sloppy

--- 2017/3.js
+++ 2017/3.js
@@ -10,19 +10,43 @@
 
 const input = require("./data").day3
 
-const nw_value  = n => Math.pow((n-1)*2, 2) + 1
-const ne_value  = n => Math.pow((n-1)*2, 2) - ((n*2)-3)
-const sw_value  = n => Math.pow((n*2)-1, 2) - ((n-1)*2)
-const se_value  = n => Math.pow((n*2)-1, 2) + 0
+const nw_value = n => Math.pow((n - 1) * 2, 2) + 1
+const ne_value = n => Math.pow((n - 1) * 2, 2) - ((n * 2) - 3)
+const sw_value = n => Math.pow((n * 2) - 1, 2) - ((n - 1) * 2)
+const se_value = n => Math.pow((n * 2) - 1, 2) + 0
 
-const diag_dist = n => (n-1)*2
 const ring = n => Math.floor(Math.ceil(Math.sqrt(n)) / 2 + 1)
-const diff_from_corner = n => {
-    const r = ring(n)
-    return [se_value(r), ne_value(r), sw_value(r), nw_value(r)]
-            .map(v => Math.abs(v - n))
-            .sort((a, b) => a > b ? 1 : -1)[0]
-}
+const diag_dist = n => (n - 1) * 2
 const distance_from = n => diag_dist(ring(n)) - diff_from_corner(n)
+const diff_from_corner = n =>
+    [se_value(ring(n)), ne_value(ring(n)), sw_value(ring(n)), nw_value(ring(n))]
+        .map(v => Math.abs(v - n))
+        .sort((a, b) => a > b ? 1 : -1)[0]
+
+const spiral_adder = () => {
+    const directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+    const neighbours = [[1, 1], [1, 0], [1, -1], [0, 1], [0, -1], [-1, 1], [-1, 0], [-1, -1]]
+
+    const grid = {}
+    let steps = 1;
+    let dir = 0;
+    let pos = [0,0]
+
+    for (let i = 0; i < 8; ++i) {
+        for (let iteration = 0; iteration < 2; iteration++) {
+            for (let step = 0; step < steps; step++) {
+                const positions = neighbours.map(c => [c[0] + pos[0], c[1] + pos[1]])
+                const value = positions.reduce((sum, next) => parseInt(sum) + (grid[next] ? parseInt(grid[next]) : 0), 0)
+                grid[pos] = pos[0] == 0 && pos[1] == 0 ? 1 : value
+                pos[0] += directions[dir][0]
+                pos[1] += directions[dir][1]
+                console.log(value)
+            }
+            dir = (dir + 1) % 4
+        }
+        ++steps;
+    }
+}
 
 console.log(distance_from(input))
+spiral_adder()
