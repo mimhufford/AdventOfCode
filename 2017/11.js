85906873663db2c0b4abd09ba3262d9e1dab1cdd
Mim Hufford <mim@hotmail.co.uk>
2017-12-11 14:25:03 +0000

    day 11 part 2

--- 2017/11.js
+++ 2017/11.js
@@ -1,40 +1,22 @@
 const input = require("./data").day11.split(',')
 
-const cancelOpposites = (steps, a, b) => {
-    if (steps[a] >= steps[b]) {
-        steps[a] -= steps[b]
-        steps[b] = 0
-    } else {
-        steps[b] -= steps[a]
-        steps[a] = 0
-    }
+const dirs = {
+    'n': [-1, 1, 0],
+    's': [1, -1, 0],
+    'ne': [0, 1, -1],
+    'nw': [-1, 0, 1],
+    'se': [1, 0, -1],
+    'sw': [0, -1, 1],
 }
 
-const cancelPart = (steps, a, b, c) => {
-    if (steps[a] >= steps[b]) {
-        steps[a] -= steps[b]
-        steps[c] += steps[b]
-        steps[b] = 0
-    }
-}
+const distance = x => Math.max(Math.abs(x[0]), Math.abs(x[1]), Math.abs(x[2]))
 
-const calculateDistance = path => {
-    const steps = { n: 0, ne: 0, se: 0, s: 0, sw: 0, nw: 0 }
-    path.forEach(dir => steps[dir]++)
-    cancelOpposites(steps, 'n', 's')
-    cancelOpposites(steps, 'ne', 'sw')
-    cancelOpposites(steps, 'nw', 'se')
-    cancelPart(steps, 'n', 'se', 'ne')
-    cancelPart(steps, 'n', 'sw', 'nw')
-    cancelPart(steps, 's', 'ne', 'se')
-    cancelPart(steps, 's', 'nw', 'sw')
-    return steps.n + steps.ne + steps.nw + steps.s + steps.se + steps.sw
-}
-    
+let pos = [0, 0, 0]
 let furthest = 0
 
-for (let i = 0; i < input.length; i++) {
-    furthest = Math.max(furthest, calculateDistance(input.slice(i)))
-}
+input.forEach(dir => {
+    pos = pos.map((x, i) => x + dirs[dir][i])
+    furthest = Math.max(furthest, distance(pos))
+})
 
-console.log(calculateDistance(input), furthest)
+console.log(distance(pos), furthest);
