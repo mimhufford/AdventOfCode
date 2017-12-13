a8a1ead5cf82f16609850cdf75411fcc25b06d03
Mim Hufford <mim@hotmail.co.uk>
2017-12-13 15:27:25 +0000

    day 12 - part 2

--- 2017/12.js
+++ 2017/12.js
@@ -5,7 +5,7 @@ const input = require("./data").day12.split('\n')
         return obj
     }, {})
 
-const howManyInGroup = (data, root) => {
+const programsInGroup = (data, root) => {
     const unchecked = [root]
     const group = new Set()
 
@@ -15,7 +15,22 @@ const howManyInGroup = (data, root) => {
         data[node].forEach(n => { if (!group.has(n)) unchecked.push(n) })
     }
 
-    return group.size
+    return group
 }
 
-console.log(howManyInGroup(input, '0'))
+const howManyGroups = data => {
+    let count = 0
+    const seen = new Set()
+
+    for (const key in data) {
+        if (!seen.has(key)) {
+            count++
+            programsInGroup(data, key).forEach(k => seen.add(k))
+        }
+    }
+
+    return count
+}
+
+console.log(programsInGroup(input, '0').size)
+console.log(howManyGroups(input))
