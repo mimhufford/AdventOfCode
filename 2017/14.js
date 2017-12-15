29c1e82e97e039e6df1c6278508f14dfaa97f8eb
Mim Hufford <mim@hotmail.co.uk>
2017-12-15 18:26:39 +0000

    day 14 part 2

--- 2017/14.js
+++ 2017/14.js
@@ -9,7 +9,7 @@ const hash = str => {
     const sparse = twist([...Array(256).keys()], input)
     const dense = []
     for (let index = 0; index < 16; index++)
-        dense.push(sparse.slice(index*16, index*16+16).reduce((a,b)=>a^b))
+        dense.push(sparse.slice(index * 16, index * 16 + 16).reduce((a, b) => a ^ b))
     const hex = dense.map(i => ('0' + i.toString(16)).substr(-2)).join("")
     return hex
 }
@@ -23,6 +23,34 @@ const twist = (data, lengths, start = 0, skip = 0) => {
     return twist(restore, lengths.slice(1), (start + length + skip) % data.length, skip + 1)
 }
 
-const grid = inputs.map(i => hex2bin(hash(i)))
+const grid = inputs.map(i => hex2bin(hash(i))).map(l => l.split(''))
 
-console.log(grid.join('').split('').filter(i => i == '1').length)
+const flood = (grid, row, col) => {
+    const check = [[row, col]]
+
+    while (check.length) {
+        const cur = check.pop()
+        grid[cur[0]][cur[1]] = '0'
+        const r = cur[0], c = cur[1]
+        const ns = [[r - 1, c], [r + 1, c], [r, c - 1], [r, c + 1]]
+            .filter(c => c[0] >= 0 && c[0] < grid.length && c[1] >= 0 && c[1] < grid.length)
+        ns.forEach(r => {
+            if (grid[r[0]][r[1]] === '1') {
+                check.push(r)
+                grid[r[0]][r[1]] = '0'
+            }
+        })
+    }
+}
+
+let groups = 0
+for (let col = 0; col < grid.length; col++) {
+    for (let row = 0; row < grid.length; row++) {
+        if (grid[row][col] === '1') {
+            flood(grid, row, col)
+            groups++
+        }
+    }
+}
+
+console.log(groups)
