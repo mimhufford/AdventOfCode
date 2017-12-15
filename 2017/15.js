d1b2978d0e6b2f2d872e2421658bb623a7184f2f
Mim Hufford <mim@hotmail.co.uk>
2017-12-15 09:35:08 +0000

    day 15 part 2

--- 2017/15.js
+++ 2017/15.js
@@ -8,8 +8,31 @@ for (let i = 0; i < 40000000; i++) {
     input[0] %= 2147483647
     input[1] %= 2147483647
 
-    if (input[0] % 65536 == input[1] % 65536) 
+    if (input[0] % 65536 == input[1] % 65536)
         matches++
 }
 
-console.log(matches)
+const generate = (start, mul, div) => {
+    const result = []
+    let val = start
+    while (result.length < 5000000) {
+        val *= mul
+        val %= 2147483647
+        if (val % div == 0) result.push(val)
+    }
+    return result
+}
+
+const judge = (a, b) => {
+    let result = 0
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] % 65536 == b[i] % 65536)
+            result++
+    }
+    return result
+}
+
+const a = generate(883, 16807, 4)
+const b = generate(879, 48271, 8)
+
+console.log(matches, judge(a, b))
