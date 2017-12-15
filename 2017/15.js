ec29019e86ddaa3994d3626d99419a612934c7ef
Mim Hufford <mim@hotmail.co.uk>
2017-12-15 21:44:26 +0000

    day 15 cleanup

--- 2017/15.js
+++ 2017/15.js
@@ -1,38 +1,14 @@
-const input = [883, 879]
-
-let matches = 0
-
-for (let i = 0; i < 40000000; i++) {
-    input[0] *= 16807
-    input[1] *= 48271
-    input[0] %= 2147483647
-    input[1] %= 2147483647
-
-    if (input[0] % 65536 == input[1] % 65536)
-        matches++
-}
-
-const generate = (start, mul, div) => {
-    const result = []
-    let val = start
-    while (result.length < 5000000) {
-        val *= mul
-        val %= 2147483647
-        if (val % div == 0) result.push(val)
-    }
-    return result
-}
-
-const judge = (a, b) => {
+const judge = (a, b, am, bm, amt) => {
     let result = 0
-    for (let i = 0; i < a.length; i++) {
-        if (a[i] % 65536 == b[i] % 65536)
-            result++
+    for (let i = 0; i < amt; i++) {
+        do { a = a * 16807 % 2147483647 } while (a % am)
+        do { b = b * 48271 % 2147483647 } while (b % bm)
+        if (a % 65536 == b % 65536) result++
     }
     return result
 }
 
-const a = generate(883, 16807, 4)
-const b = generate(879, 48271, 8)
-
-console.log(matches, judge(a, b))
+console.log(
+    judge(883, 879, 1, 1, 40000000),
+    judge(883, 879, 4, 8, 5000000)
+)
