1634c0093fe5ec83774de8809336ab35c5503db1
Mim Hufford <mim@hotmail.co.uk>
2017-12-17 14:07:59 +0000

    day 17 part 2

--- 2017/17.js
+++ 2017/17.js
@@ -2,12 +2,19 @@ const input = 369
 
 const d = {
     buffer: [0],
-    index: 0,
+    p1i: 0,
+    p2i: 0,
+    p2r: 0,
 }
 
 for (let i = 0; i < 2017; i++) {
-    d.index = (d.index + input) % d.buffer.length
-    d.buffer.splice(++d.index, 0, d.buffer.length)
+    d.p1i = (d.p1i + input) % d.buffer.length
+    d.buffer.splice(++d.p1i, 0, d.buffer.length)
 }
 
-console.log(d.buffer[d.buffer.indexOf(2017) + 1])
+for (let i = 1; i <= 50000000; i++) {
+    d.p2i = (d.p2i + 1 + input) % i
+    if (d.p2i === 0) d.p2r = i
+}
+
+console.log(d.buffer[d.buffer.indexOf(2017) + 1], d.p2r)
