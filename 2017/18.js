6494763da464e8db5bb9cac8a0b1cc42b1e83943
Mim Hufford <mim@hotmail.co.uk>
2017-12-18 14:18:24 +0000

    day 18 part 2

--- 2017/18.js
+++ 2017/18.js
@@ -9,13 +9,13 @@ const state = {
 const vor = (thread, x) => thread.regs[x] ? thread.regs[x] : Number(x)
 
 const instructions = {
-    'jgz': (a, _) => (x, y) => a.ip += vor(a, x) > 0 ? vor(a, y) - 1 : 0,
-    'set': (a, _) => (r, x) => a.regs[r] = vor(a, x),
-    'add': (a, _) => (r, x) => a.regs[r] += vor(a, x),
-    'mul': (a, _) => (r, x) => a.regs[r] *= vor(a, x),
-    'mod': (a, _) => (r, x) => a.regs[r] %= vor(a, x),
-    'snd': (a, b) => (x, _) => b.queue.push(vor(a, x)),
-    'rcv': (a, _) => (r, _) => a.regs[r] = a.queue.shift(),
+    'jgz': a => _ => x => y => a.ip += vor(a, x) > 0 ? vor(a, y) - 1 : 0,
+    'set': a => _ => r => x => a.regs[r] = vor(a, x),
+    'add': a => _ => r => x => a.regs[r] += vor(a, x),
+    'mul': a => _ => r => x => a.regs[r] *= vor(a, x),
+    'mod': a => _ => r => x => a.regs[r] %= vor(a, x),
+    'snd': a => b => x => _ => b.queue.push(vor(a, x)),
+    'rcv': a => _ => r => _ => a.regs[r] = a.queue.shift(),
 }
 
 const runProgram = s => {
@@ -23,7 +23,7 @@ const runProgram = s => {
         if (ta.ip >= 0 && ta.ip < s.program.length) {
             const [i, a, b] = s.program[ta.ip].split(' ')
             if ((i === 'rcv' && ta.queue.length > 0) || i !== 'rcv') {
-                instructions[i](ta, tb)(a, b)
+                instructions[i](ta)(tb)(a)(b)
                 ta.ip++
                 ta.sent += i === 'snd' ? 1 : 0
                 return true
