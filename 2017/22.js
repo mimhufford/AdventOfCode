644c6a317abaf93f3516417db7d23739cde84eda
Mim Hufford <mim@hotmail.co.uk>
2017-12-22 08:22:41 +0000

    day 22 part 2

--- 2017/22.js
+++ 2017/22.js
@@ -13,7 +13,7 @@ const virus = {
 
 const get = (grid, row, col) => {
     grid[row] = grid[row] || {}
-    return grid[row][col]
+    return grid[row][col] || 0
 }
 
 const set = (grid, row, col, val) => {
@@ -22,23 +22,31 @@ const set = (grid, row, col, val) => {
 }
 
 input.forEach((str, row) => str.split('').forEach((chr, col) => {
-    set(grid, row, col, chr === '#')
+    set(grid, row, col, chr === '#' ? 2 : 0)
 }))
 
-const burst = (grid, virus) => {
-    const infected = get(grid, virus.r, virus.c)
-    set(grid, virus.r, virus.c, !infected)
+const v1 = (grid, virus) => {
+    const infected = get(grid, virus.r, virus.c) === 2
+    set(grid, virus.r, virus.c, infected ? 0 : 2)
     virus.i += infected ? 0 : 1
     virus.d = infected ? virus.d.r : virus.d.l
     virus.r += virus.d.y
     virus.c += virus.d.x
 }
 
-const doBursts = (amount, grid, virus) => {
-    for (let i = 0; i < amount; i++) {
-        burst(grid, virus)
-    }
+const v2 = (grid, virus) => {
+    const state = get(grid, virus.r, virus.c)
+    set(grid, virus.r, virus.c, (state + 1) % 4)
+    virus.i += state == 1 ? 1 : 0
+    virus.d = state == 0 ? virus.d.l : state == 2 ? virus.d.r : state == 3 ? virus.d.r.r : virus.d
+    virus.r += virus.d.y
+    virus.c += virus.d.x
 }
 
-doBursts(10000, grid, virus)
+const doBursts = (burstFn, amount, grid, virus) => {
+    for (let i = 0; i < amount; i++) burstFn(grid, virus)
+}
+
+//doBursts(v1, 10000, grid, virus)
+doBursts(v2, 10000000, grid, virus)
 console.log(virus.i)
