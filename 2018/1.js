31518e43b310d0274dc8ac45a1d6c91070a57022
Mim Hufford <mim@hotmail.co.uk>
2018-12-01 08:53:33 +0000

    1.2, 3x speed increase

--- 2018/1.js
+++ 2018/1.js
@@ -4,10 +4,10 @@ const deltas = input.split('\n').map(Number)
 
 console.log("Part 1:", deltas.reduce((p, c) => p + c))
 
-for (let freq = 0, i = 0, seen = {}; ; i = (i + 1) % deltas.length) {
-    seen[freq] = seen[freq] + 1 || 1
+for (let freq = 0, i = 0, freqs = new Set(); ; i = (i + 1) % deltas.length) {
+    freqs.add(freq)
     freq += deltas[i]
-    if (seen[freq] === 1) {
+    if (freqs.has(freq)) {
         console.log("Part 2:", freq)
         break
     }
