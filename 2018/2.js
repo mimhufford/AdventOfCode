8428e2aa747c7fa710647d506581176abfa5b6ce
Mim Hufford <mim@hotmail.co.uk>
2018-12-02 12:12:24 +0000

    2018 2.2

--- 2018/2.js
+++ 2018/2.js
@@ -8,3 +8,23 @@ const match2 = freqs.filter(f => new Set(Object.values(f)).has(2))
 const match3 = freqs.filter(f => new Set(Object.values(f)).has(3))
 
 console.log("Part 1:", match2.length * match3.length)
+
+const offBy1 = (a, b) => {
+    for (let diff = 0, i = 0; i < a.length; i++) {
+        if (a[i] != b[i]) diff++
+        if (diff > 1) return false
+    }
+    return true
+}
+
+for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+        if (offBy1(input[i], input[j])) {
+            let result = ''
+            for (let c = 0; c < input[i].length; c++)
+                if (input[i][c] == input[j][c]) result += input[i][c]
+            console.log("Part 2:", result)
+            return;
+        }
+    }
+}
