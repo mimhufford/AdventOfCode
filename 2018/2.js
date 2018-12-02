ce65892da7bf268c3b479f9d556d40aacfbdac68
Mim Hufford <mim@hotmail.co.uk>
2018-12-02 09:32:55 +0000

    Simplify 2018 2.1

--- 2018/2.js
+++ 2018/2.js
@@ -1,14 +1,10 @@
 const input = require("./data").day2.split('\n')
 
-const freqs = input.reduce((obj, id) => {
-    obj[id] = id.split('').reduce((f, c) => {
-        f[c] = (f[c] || 0) + 1
-        return f
-    }, {})
-    return obj
-}, {})
+const freqs = input.map(id => id.split('').reduce((f, c) => {
+    f[c] = (f[c] || 0) + 1; return f
+}, {}))
 
-const match2 = Object.keys(freqs).filter(id => new Set(Object.values(freqs[id])).has(2))
-const match3 = Object.keys(freqs).filter(id => new Set(Object.values(freqs[id])).has(3))
+const match2 = freqs.filter(f => new Set(Object.values(f)).has(2))
+const match3 = freqs.filter(f => new Set(Object.values(f)).has(3))
 
 console.log("Part 1:", match2.length * match3.length)
