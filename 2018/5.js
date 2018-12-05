d40cc036e2da7d4a40f446b817e69ac0e12c7ae2
Mim Hufford <mim@hotmail.co.uk>
2018-12-05 09:14:05 +0000

    2018 5.2

--- 2018/5.js
+++ 2018/5.js
@@ -1,15 +1,20 @@
-const input = require("./data").day5
+const input = require("./data").day5.split('')
 
-const match = (a, b) => a && a !== b && a.toUpperCase() === b.toUpperCase()
+const react = remove => {
+    const match = (a, b) => a && a !== b && a.toUpperCase() === b.toUpperCase()
+    const filtered = input.filter(unit => unit.toUpperCase() !== remove)
+    const result = [filtered[0]]
 
-const result = [input[0]]
+    for (let i = 1; i < filtered.length - 1; i++) {
+        const a = result[result.length - 1]
+        const b = filtered[i]
 
-for (let i = 1; i < input.length - 1; i++) {
-    const a = result[result.length - 1]
-    const b = input[i]
+        if (match(a, b)) result.pop()
+        else             result.push(b)
+    }
 
-    if (match(a, b)) result.pop()
-    else             result.push(b)
+    return (result.join('') + filtered[filtered.length - 1]).length
 }
 
-console.log("Part 1:", (result.join('') + input[input.length - 1]).length)
+console.log("Part 1:", react(''))
+console.log("Part 2:", 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(c => react(c)).sort()[0])
