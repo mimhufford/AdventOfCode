e2b7977c29872be2e0494ca4fc9085469bc50d4c
Mim Hufford <mim@hotmail.co.uk>
2018-12-06 08:59:18 +0000

    2018 6.2

--- 2018/6.js
+++ 2018/6.js
@@ -6,15 +6,16 @@ const dist = (x, y, coord) => Math.abs(x - coord[0]) + Math.abs(y - coord[1])
 
 const cells = []
 const infinites = new Set()
+let safe = 0
 
-const x0 = coords.reduce((res, cur) => Math.min(res, cur[0]), Number.MAX_VALUE)
-const x1 = coords.reduce((res, cur) => Math.max(res, cur[0]), Number.MIN_VALUE)
-const y0 = coords.reduce((res, cur) => Math.min(res, cur[1]), Number.MAX_VALUE)
-const y1 = coords.reduce((res, cur) => Math.max(res, cur[1]), Number.MIN_VALUE)
+const [x0, x1, y0, y1] = coords.reduce((r, c) =>
+    [Math.min(r[0], c[0]), Math.max(r[1], c[0]), Math.min(r[2], c[1]), Math.max(r[3], c[1])],
+    [Number.MAX_VALUE, Number.MIN_VALUE, Number.MAX_VALUE, Number.MIN_VALUE])
 
 for (let y = y0; y <= y1; y++) {
     for (let x = x0; x <= x1; x++) {
         const distances = coords.map((c, i) => [i, dist(x, y, c)]).sort((a, b) => a[1] > b[1] ? 1 : -1)
+        if (distances.map(d => d[1]).reduce((a, b) => a + b) < 10000) safe++
         if (distances[0][1] !== distances[1][1]) {
             cells.push(distances[0][0])
             if (x === x0 || y === y0 || x === x1 || y === y1)
@@ -31,3 +32,4 @@ const counts = cells.reduce((res, cur) => {
 }, {})
 
 console.log("Part 1:", Object.values(counts).sort((a, b) => a < b ? 1 : -1)[0])
+console.log("Part 2:", safe)
