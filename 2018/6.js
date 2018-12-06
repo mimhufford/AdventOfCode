022d660101aaf5f87b69af8d9fdc4139cdd905ff
Mim Hufford <mim@hotmail.co.uk>
2018-12-06 09:43:05 +0000

    2018 6 - optimised as much as I can be bothered

--- 2018/6.js
+++ 2018/6.js
@@ -1,11 +1,9 @@
-const input = require("./data").day6
+const input = require("./data").day6.split('\n').map(l => l.split(', ').map(Number))
 
-const coords = input.split('\n').map(l => l.split(', ').map(Number))
-
-const cells = []
+const cells = Array(input.length).fill(0)
 let safe = 0
 
-const [x0, x1, y0, y1] = coords.reduce((r, c) =>
+const [x0, x1, y0, y1] = input.reduce((r, c) =>
     [Math.min(r[0], c[0]), Math.max(r[1], c[0]), Math.min(r[2], c[1]), Math.max(r[3], c[1])],
     [Number.MAX_VALUE, Number.MIN_VALUE, Number.MAX_VALUE, Number.MIN_VALUE])
 
@@ -14,8 +12,8 @@ for (let y = y0; y <= y1; y++) {
         let lowestIndex = []
         let lowestDistance = Number.MAX_VALUE
         let totalDistance = 0
-        for (let i = 0; i < coords.length; i++) {
-            const distance = Math.abs(x - coords[i][0]) + Math.abs(y - coords[i][1])
+        for (let i = 0; i < input.length; i++) {
+            const distance = Math.abs(x - input[i][0]) + Math.abs(y - input[i][1])
             totalDistance += distance
             if (distance < lowestDistance) {
                 lowestIndex = [i]
@@ -26,13 +24,10 @@ for (let y = y0; y <= y1; y++) {
         }
         if (totalDistance < 10000) safe++
         if (lowestIndex.length === 1) {
-            const index = lowestIndex[0]
             if (x === x0 || y === y0 || x === x1 || y === y1)
-                cells[index] = Number.POSITIVE_INFINITY
-            else {
-                cells[index] = cells[index] || 0
-                cells[index]++
-            }
+                cells[lowestIndex[0]] = Number.POSITIVE_INFINITY
+            else
+                cells[lowestIndex[0]]++
         }
     }
 }
