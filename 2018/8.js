3f929b02c8bc6ed79cbb7d09ce0c604d78a9276d
Mim Hufford <mim@hotmail.co.uk>
2018-12-08 09:12:45 +0000

    2018 8.2 - can probably build tree and calc values in one pass

--- 2018/8.js
+++ 2018/8.js
@@ -1,5 +1,7 @@
 const input = require("./data").day8.split(' ').map(Number)
 
+const sum = (a, b) => a + b
+
 const build = data => {
     const numChildren = data.shift()
     const numMetadata = data.shift()
@@ -9,9 +11,24 @@ const build = data => {
     }
 }
 
-const tree = build(input)
+const calcValues = node => {
+    if (node.children.length === 0)
+        node.value = node.metadata.reduce(sum)
+    else {
+        let value = 0
+        for (const i of node.metadata) {
+            const child = node.children[i - 1]
+            if (child) value += calcValues(child)
+        }
+        node.value = value
+    }
+    return node.value
+}
 
-const sum = (a, b) => a + b
 const metasum = node => node.metadata.reduce(sum) + node.children.map(metasum).reduce(sum, 0)
 
+const tree = build(input)
+calcValues(tree)
+
 console.log("Part 1:", metasum(tree))
+console.log("Part 2:", tree.value)
