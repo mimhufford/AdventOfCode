6394509f5a80787b31bf8d8b26d720d275d11441
Mim Hufford <mim@hotmail.co.uk>
2018-12-11 23:01:30 +0000

    2018 11.1

---
+++ 2018/11.js
@@ -0,0 +1,27 @@
+const findLargestPosition = serial => {
+    const grid = []
+    for (let y = 1; y <= 300; y++) {
+        grid[y] = []
+        for (let x = 1; x <= 300; x++)
+            grid[y][x] = Math.floor(((x + 10) * y + serial) * (x + 10) / 100) % 10 - 5
+    }
+
+    let largest = Number.NEGATIVE_INFINITY
+    let largestPos = []
+
+    for (let y = 1; y <= 298; y++) {
+        for (let x = 1; x <= 298; x++) {
+            cumlative = 0
+            for (gy = y; gy < y + 3; gy++)
+                for (gx = x; gx < x + 3; gx++)
+                    cumlative += grid[gy][gx]
+            if (cumlative > largest) {
+                largest = cumlative
+                largestPos = [x, y]
+            }
+        }
+    }
+    return `${largestPos[0]},${largestPos[1]}`
+}
+
+console.log("Part 1:", findLargestPosition(5719))
