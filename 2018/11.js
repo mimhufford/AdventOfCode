9cb95621208ff167e683bce63292f9eea623d9f2
Mim Hufford <mim@hotmail.co.uk>
2018-12-11 23:29:16 +0000

    2018 11.2 - hack but gives the right answer

--- 2018/11.js
+++ 2018/11.js
@@ -6,22 +6,45 @@ const findLargestPosition = serial => {
             grid[y][x] = Math.floor(((x + 10) * y + serial) * (x + 10) / 100) % 10 - 5
     }
 
-    let largest = Number.NEGATIVE_INFINITY
-    let largestPos = []
+    {
+        let largest = Number.NEGATIVE_INFINITY
+        let largestPos = []
 
-    for (let y = 1; y <= 298; y++) {
-        for (let x = 1; x <= 298; x++) {
-            cumlative = 0
-            for (let gy = y; gy < y + 3; gy++)
-                for (let gx = x; gx < x + 3; gx++)
-                    cumlative += grid[gy][gx]
-            if (cumlative > largest) {
-                largest = cumlative
-                largestPos = [x, y]
+        const size = 3
+        for (let y = 1; y <= 301 - size; y++) {
+            for (let x = 1; x <= 301 - size; x++) {
+                cumlative = 0
+                for (let gy = y; gy < y + size; gy++)
+                    for (let gx = x; gx < x + size; gx++)
+                        cumlative += grid[gy][gx]
+                if (cumlative > largest) {
+                    largest = cumlative
+                    largestPos = [x, y, size]
+                }
+            }
+        }
+        console.log(`Part 1: ${largestPos[0]},${largestPos[1]}`)
+    }
+    {
+        let largest = Number.NEGATIVE_INFINITY
+        let largestPos = []
+
+        for (let size = 1; size < 300; size++) {
+            for (let y = 1; y <= 301 - size; y++) {
+                for (let x = 1; x <= 301 - size; x++) {
+                    cumlative = 0
+                    for (let gy = y; gy < y + size; gy++)
+                        for (let gx = x; gx < x + size; gx++)
+                            cumlative += grid[gy][gx]
+                    if (cumlative > largest) {
+                        largest = cumlative
+                        largestPos = [x, y, size]
+                        console.log(`Part 2: ${largestPos[0]},${largestPos[1]},${largestPos[2]}`)
+                    }
+                }
             }
         }
     }
-    return `${largestPos[0]},${largestPos[1]}`
 }
 
-console.log("Part 1:", findLargestPosition(5719))
+findLargestPosition(5719)
