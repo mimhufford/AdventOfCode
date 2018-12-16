107dcc26eb29c363238c0d124a5407490520451d
Mim Hufford <mim@hotmail.co.uk>
2018-12-16 20:38:45 +0000

    2018 11 - fully optimised

--- 2018/11.js
+++ 2018/11.js
@@ -1,19 +1,13 @@
 const serial = 5719
 
-const grid = []
+const cumulative = []
 for (let y = 1; y <= 300; y++) {
-    grid[y] = []
-    for (let x = 1; x <= 300; x++)
-        grid[y][x] = Math.floor(((x + 10) * y + serial) * (x + 10) / 100) % 10 - 5
-}
-
-const cumlative = []
-for (let y = 1; y <= 300; y++) {
-    cumlative[y] = []
+    cumulative[y] = []
     for (let x = 1; x <= 300; x++) {
-        cumlative[y][x] = Math.floor(((x + 10) * y + serial) * (x + 10) / 100) % 10 - 5
-        if (x === 1 || y === 1) continue
-        cumlative[y][x] += cumlative[y][x - 1] + cumlative[y - 1][x] + cumlative[y - 1][x - 1]
+        cumulative[y][x] = Math.floor(((x + 10) * y + serial) * (x + 10) / 100) % 10 - 5
+        if (x > 1) cumulative[y][x] += cumulative[y][x - 1]
+        if (y > 1) cumulative[y][x] += cumulative[y - 1][x]
+        if (y > 1 && x > 1) cumulative[y][x] -= cumulative[y - 1][x - 1]
     }
 }
 
@@ -21,21 +15,18 @@ const largestForSize = (min, max) => {
     let largest = Number.NEGATIVE_INFINITY
     let largestPos = []
     for (let size = min; size < (max || min) + 1; size++) {
-        for (let y = 1; y <= 301 - size; y++) {
-            for (let x = 1; x <= 301 - size; x++) {
-                cumlative = 0
-                for (let gy = y; gy < y + size; gy++)
-                    for (let gx = x; gx < x + size; gx++)
-                        cumlative += grid[gy][gx]
-                if (cumlative > largest) {
-                    largest = cumlative
+        for (let y = 1; y <= 300 - size; y++) {
+            for (let x = 1; x <= 300 - size; x++) {
+                const sum = cumulative[y][x] + cumulative[y + size][x + size] -
+                    cumulative[y][x + size] - cumulative[y + size][x]
+                if (sum > largest) {
+                    largest = sum
                     largestPos = [x, y, size]
-                    console.log(`${largestPos[0]},${largestPos[1]},${largestPos[2]}`)
                 }
             }
         }
     }
-    return `${largestPos[0]},${largestPos[1]}${max ? `,${largestPos[2]}` : ''}`
+    return `${largestPos[0] + 1},${largestPos[1] + 1}${max ? `,${largestPos[2]}` : ''}`
 }
 
 console.log("Part 1:", largestForSize(3))
