c6504e4f51c4ae30e891bc51a86f7545fe06e8c0
Mim Hufford <mim@hotmail.co.uk>
2018-12-16 12:59:34 +0000

    2018 16

--- 2018/16.js
+++ 2018/16.js
@@ -19,40 +19,45 @@ const ops = [
     eqrr = (r, a, b, c) => r[c] = r[a] == r[b] ? 1 : 0,
 ]
 
-const analyseSamples = () => {
-    const idToOp = []
-    let threeOrMore = 0
-    for (let line = 0; line < input.length; line++) {
-        if (input[line][0] === 'B') {
-            const before = input[line++].match(/\d+/g).map(Number)
-            const instruction = input[line++].match(/\d+/g).map(Number)
-            const after = input[line++].match(/\d+/g).map(Number)
-            let matches = 0
-            for (const op of ops) {
-                const result = [...before]
-                const [i, a, b, c] = instruction
-                op(result, a, b, c)
-                if (result.every((_, i) => result[i] === after[i])) {
-                    matches++
-                    idToOp[i] = idToOp[i] || new Set()
-                    idToOp[i].add(op)
-                }
+const idToOp = []
+let threeOrMore = 0
+for (let line = 0; line < input.length; line++) {
+    if (input[line][0] === 'B') {
+        const before = input[line++].match(/\d+/g).map(Number)
+        const instruction = input[line++].match(/\d+/g).map(Number)
+        const after = input[line++].match(/\d+/g).map(Number)
+        let matches = 0
+        for (const op of ops) {
+            const result = [...before]
+            const [i, a, b, c] = instruction
+            op(result, a, b, c)
+            if (result.every((_, i) => result[i] === after[i])) {
+                matches++
+                idToOp[i] = idToOp[i] || new Set()
+                idToOp[i].add(op)
             }
-            if (matches >= 3) threeOrMore++
         }
+        if (matches >= 3) threeOrMore++
     }
-
-    while (!idToOp.every(s => s.size === 1)) {
-        const solvedOps = idToOp.filter(s => s.size === 1).map(s => Array.from(s)[0])
-        for (const unsolvedOp of idToOp)
-            if (unsolvedOp.size > 1)
-                for (const op of solvedOps)
-                    unsolvedOp.delete(op)
-    }
-
-    const sortedOps = idToOp.map(set => Array.from(set)[0])
-    return { threeOrMore, sortedOps }
 }
 
-const result = analyseSamples()
-console.log("Part 1:", result.threeOrMore)
+while (!idToOp.every(s => s.size === 1)) {
+    const solvedOps = idToOp.filter(s => s.size === 1).map(s => Array.from(s)[0])
+    for (const unsolvedOp of idToOp)
+        if (unsolvedOp.size > 1)
+            for (const op of solvedOps)
+                unsolvedOp.delete(op)
+}
+
+const sortedOps = idToOp.map(set => Array.from(set)[0])
+
+const registers = [0, 0, 0, 0]
+for (let line = 0; line < input.length; line++) {
+    if (input[line][0] === 'B') { line += 3; continue }
+    if (input[line].length === 0) continue
+    const [i, a, b, c] = input[line].match(/\d+/g).map(Number)
+    sortedOps[i](registers, a, b, c)
+}
+
+console.log("Part 1:", threeOrMore)
+console.log("Part 2:", registers[0])
