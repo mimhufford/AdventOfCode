b32dd67233e3770a29d1e73b376e66f9d88079a5
Mim Hufford <mim@hotmail.co.uk>
2018-12-20 10:37:02 +0000

    2018 20

--- 2018/20.js
+++ 2018/20.js
@@ -3,17 +3,17 @@ const input = require("./data").day20
 const map = {}
 const pStack = []
 const sStack = []
-let x = 0, y = 0, steps = 0
+let x = 0, y = 0, s = 0
 
 for (const c of input.substring(1, input.length - 1)) {
     switch (c) {
-        case 'N': map[`${x},${--y}`] = Math.min(map[`${x},${y}`] || 99999, ++steps); break
-        case 'E': map[`${++x},${y}`] = Math.min(map[`${x},${y}`] || 99999, ++steps); break
-        case 'S': map[`${x},${++y}`] = Math.min(map[`${x},${y}`] || 99999, ++steps); break
-        case 'W': map[`${--x},${y}`] = Math.min(map[`${x},${y}`] || 99999, ++steps); break
-        case '(': pStack.push([x, y]); sStack.push(steps); break
-        case ')': [x, y] = pStack.pop(); steps = sStack.pop(); break
-        case '|': [x, y] = pStack[pStack.length - 1]; steps = sStack[sStack.length - 1]; break
+        case 'N': map[`${x},${--y}`] = Math.min(map[`${x},${y}`] || 99999, ++s); break
+        case 'E': map[`${++x},${y}`] = Math.min(map[`${x},${y}`] || 99999, ++s); break
+        case 'S': map[`${x},${++y}`] = Math.min(map[`${x},${y}`] || 99999, ++s); break
+        case 'W': map[`${--x},${y}`] = Math.min(map[`${x},${y}`] || 99999, ++s); break
+        case '(': pStack.push([x, y]); sStack.push(s); break
+        case ')': [x, y] = pStack.pop(); s = sStack.pop(); break
+        case '|': [x, y] = pStack[pStack.length - 1]; s = sStack[sStack.length - 1]
     }
 }
 
