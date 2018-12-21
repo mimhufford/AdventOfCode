3ea6c3d9384c7acc22be8e2e7f73e357b05c301a
Mim Hufford <mim@hotmail.co.uk>
2018-12-21 09:52:19 +0000

    2018 21 - more simplification

--- 2018/21.js
+++ 2018/21.js
@@ -1,37 +1,23 @@
 let a = 0
 let b = 0
 let c = 0
-let d = 0
 let e = 0
 
-c = 123
-while (true) {
-    c = c & 456
-    c = c == 72
-    if (c) break
-}
-c = 0
 while (true) {
     b = c | 65536
     c = 10373714
     while (true) {
-        e = b & 255
-        c += e
+        c = c + (b & 255)
         c = c & 16777215
         c *= 65899
         c = c & 16777215
-        e = 256 > b
-        if (e) break
+        if (256 > b) break
         e = 0
         while (true) {
-            d = e + 1
-            d *= 256
-            d = d > b
-            if (d) break
+            if ((e + 1) * 256 > b) break
             e += 1
         }
         b = e
     }
-    e = c == a
-    if (e) break
+    if (c == a) break
 }
