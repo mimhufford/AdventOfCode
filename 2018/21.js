67d4a5edb841acd74c4e77a37ce8893531a67f52
Mim Hufford <mim@hotmail.co.uk>
2018-12-21 09:46:12 +0000

    2018 21

--- 2018/21.js
+++ 2018/21.js
@@ -1,30 +1,37 @@
 let a = 0
 let b = 0
-let c = 72
+let c = 0
 let d = 0
 let e = 0
 
-do {
+c = 123
+while (true) {
+    c = c & 456
+    c = c == 72
+    if (c) break
+}
+c = 0
+while (true) {
     b = c | 65536
     c = 10373714
-    do {
+    while (true) {
         e = b & 255
         c += e
-        c &= 16777215
+        c = c & 16777215
         c *= 65899
-        c &= 16777215
+        c = c & 16777215
         e = 256 > b
         if (e) break
         e = 0
-        do {
-            d += 5
+        while (true) {
+            d = e + 1
             d *= 256
             d = d > b
             if (d) break
             e += 1
-        } while (true)
+        }
         b = e
-    } while (true)
+    }
     e = c == a
     if (e) break
-} while (true)
+}
