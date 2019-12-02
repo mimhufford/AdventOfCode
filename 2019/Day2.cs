4e02c8571e33bfc8e5050cec359bd151d1588271
Mim Hufford <mim@hotmail.co.uk>
2019-12-02 09:47:16 +0000

    2019 day 2 refactor

--- 2019/Day2.cs
+++ 2019/Day2.cs
@@ -7,44 +7,36 @@ namespace AoC
     {
         public Day2() : base(2) { }
 
-        protected override void Solve()
+        int Run(int noun, int verb)
         {
             var memory = IntCSV.ToArray();
             var ip = 0;
 
-            // part 1
-            memory[1] = 12;
-            memory[2] = 2;
+            memory[1] = noun;
+            memory[2] = verb;
 
             while (memory[ip] != 99)
             {
-                if (memory[ip] == 1) memory[memory[ip + 3]] = memory[memory[ip + 1]] + memory[memory[ip + 2]];
-                else if (memory[ip] == 2) memory[memory[ip + 3]] = memory[memory[ip + 1]] * memory[memory[ip + 2]];
+                if (memory[ip] == 1)
+                    memory[memory[ip + 3]] = memory[memory[ip + 1]] + memory[memory[ip + 2]];
+                else if (memory[ip] == 2)
+                    memory[memory[ip + 3]] = memory[memory[ip + 1]] * memory[memory[ip + 2]];
 
                 ip += 4;
             }
 
-            Part1 = memory[0].ToString();
+            return memory[0];
+        }
 
-            // part 2
+        protected override void Solve()
+        {
+            Part1 = Run(12, 2).ToString();
+
             for (int noun = 0; noun < 100; noun++)
             {
                 for (int verb = 0; verb < 100; verb++)
                 {
-                    memory = IntCSV.ToArray();
-                    ip = 0;
-                    memory[1] = noun;
-                    memory[2] = verb;
-
-                    while (memory[ip] != 99)
-                    {
-                        if (memory[ip] == 1) memory[memory[ip + 3]] = memory[memory[ip + 1]] + memory[memory[ip + 2]];
-                        else if (memory[ip] == 2) memory[memory[ip + 3]] = memory[memory[ip + 1]] * memory[memory[ip + 2]];
-
-                        ip += 4;
-                    }
-
-                    if (memory[0] == 19690720)
+                    if (Run(noun, verb) == 19690720)
                     {
                         Part2 = (100 * noun + verb).ToString();
                         noun = verb = 100; // break loops
