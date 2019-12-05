5320c73021cf4fffe4aef53ce94140a4863ff46c
Mim Hufford <mim@hotmail.co.uk>
2019-12-05 08:59:22 +0000

    2019 day 5 optimise

--- 2019/Day5.cs
+++ 2019/Day5.cs
@@ -7,17 +7,6 @@ namespace AoC
     {
         public Day5() : base(5) { }
 
-        (int op, int[] mode) ParseOpcode(int opcode)
-        {
-            var op = opcode % 10;
-            var mode = (opcode / 100).ToString("D3")
-                                     .ToCharArray()
-                                     .Reverse()
-                                     .Select(c => int.Parse(c.ToString()))
-                                     .ToArray();
-            return (op, mode);
-        }
-
         int Run(int input)
         {
             var memory = IntCSV.ToArray();
@@ -26,19 +15,22 @@ namespace AoC
 
             while (memory[ip] != 99)
             {
-                (var op, var mode) = ParseOpcode(memory[ip]);
+                var op = memory[ip] % 10;
+                var mode0 = (memory[ip] / 100) % 10;
+                var mode1 = memory[ip] / 1000;
+
                 if (op == 1)
                 {
-                    var p1 = mode[0] == 0 ? memory[memory[ip + 1]] : memory[ip + 1];
-                    var p2 = mode[1] == 0 ? memory[memory[ip + 2]] : memory[ip + 2];
+                    var p1 = mode0 == 0 ? memory[memory[ip + 1]] : memory[ip + 1];
+                    var p2 = mode1 == 0 ? memory[memory[ip + 2]] : memory[ip + 2];
                     var dest = memory[ip + 3];
                     memory[dest] = p2 + p1;
                     ip += 4;
                 }
                 else if (op == 2)
                 {
-                    var p1 = mode[0] == 0 ? memory[memory[ip + 1]] : memory[ip + 1];
-                    var p2 = mode[1] == 0 ? memory[memory[ip + 2]] : memory[ip + 2];
+                    var p1 = mode0 == 0 ? memory[memory[ip + 1]] : memory[ip + 1];
+                    var p2 = mode1 == 0 ? memory[memory[ip + 2]] : memory[ip + 2];
                     var dest = memory[ip + 3];
                     memory[dest] = p2 * p1;
                     ip += 4;
@@ -51,35 +43,35 @@ namespace AoC
                 }
                 else if (op == 4)
                 {
-                    result = mode[0] == 0 ? memory[memory[ip + 1]] : memory[ip + 1];
+                    result = mode0 == 0 ? memory[memory[ip + 1]] : memory[ip + 1];
                     ip += 2;
                 }
                 else if (op == 5)
                 {
-                    var p1 = mode[0] == 0 ? memory[memory[ip + 1]] : memory[ip + 1];
-                    var p2 = mode[1] == 0 ? memory[memory[ip + 2]] : memory[ip + 2];
+                    var p1 = mode0 == 0 ? memory[memory[ip + 1]] : memory[ip + 1];
+                    var p2 = mode1 == 0 ? memory[memory[ip + 2]] : memory[ip + 2];
                     if (p1 != 0) ip = p2;
                     else ip += 3;
                 }
                 else if (op == 6)
                 {
-                    var p1 = mode[0] == 0 ? memory[memory[ip + 1]] : memory[ip + 1];
-                    var p2 = mode[1] == 0 ? memory[memory[ip + 2]] : memory[ip + 2];
+                    var p1 = mode0 == 0 ? memory[memory[ip + 1]] : memory[ip + 1];
+                    var p2 = mode1 == 0 ? memory[memory[ip + 2]] : memory[ip + 2];
                     if (p1 == 0) ip = p2;
                     else ip += 3;
                 }
                 else if (op == 7)
                 {
-                    var p1 = mode[0] == 0 ? memory[memory[ip + 1]] : memory[ip + 1];
-                    var p2 = mode[1] == 0 ? memory[memory[ip + 2]] : memory[ip + 2];
+                    var p1 = mode0 == 0 ? memory[memory[ip + 1]] : memory[ip + 1];
+                    var p2 = mode1 == 0 ? memory[memory[ip + 2]] : memory[ip + 2];
                     var dest = memory[ip + 3];
                     memory[dest] = p1 < p2 ? 1 : 0;
                     ip += 4;
                 }
                 else if (op == 8)
                 {
-                    var p1 = mode[0] == 0 ? memory[memory[ip + 1]] : memory[ip + 1];
-                    var p2 = mode[1] == 0 ? memory[memory[ip + 2]] : memory[ip + 2];
+                    var p1 = mode0 == 0 ? memory[memory[ip + 1]] : memory[ip + 1];
+                    var p2 = mode1 == 0 ? memory[memory[ip + 2]] : memory[ip + 2];
                     var dest = memory[ip + 3];
                     memory[dest] = p1 == p2 ? 1 : 0;
                     ip += 4;
