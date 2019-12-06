dd3e0c54e84a4cc68d5db73037c96de6083ba4a7
mimhufford <mim@hotmail.co.uk>
2019-12-06 10:25:53 +0000

    2019 day 6 part 2

--- 2019/Day6.cs
+++ 2019/Day6.cs
@@ -21,11 +21,11 @@ namespace AoC
             int orbits = 0;
             foreach (var node in Lines)
             {
-                var parts = node.Split(')');
+                var parts  = node.Split(')');
                 var parent = parts[0];
-                var child = parts[1];
+                var child  = parts[1];
                 if (!nodes.ContainsKey(parent)) nodes.Add(parent, new Node());
-                if (!nodes.ContainsKey(child)) nodes.Add(child, new Node());
+                if (!nodes.ContainsKey(child))  nodes.Add(child,  new Node());
                 nodes[child].parent = nodes[parent];
             }
 
@@ -41,6 +41,42 @@ namespace AoC
             }
 
             Part1 = orbits.ToString();
+
+            List<Node> AncestorsOf(Node n)
+            {
+                var ancestors = new List<Node>();
+                while (n.parent != null)
+                {
+                    ancestors.Add(n.parent);
+                    n = n.parent;
+                }
+                return ancestors;
+            }
+
+            Node CommonAncestorOf(Node a, Node b)
+            {
+                var aAs = AncestorsOf(a);
+                var bAs = AncestorsOf(b);
+                int jumps = 0;
+                while (!bAs.Contains(aAs[jumps])) jumps++;
+                return aAs[jumps];
+            }
+
+            int DistanceFromTo(Node a, Node b)
+            {
+                int jumps = 0;
+                while (a.parent != b)
+                {
+                    jumps += 1;
+                    a = a.parent;
+                }
+                return jumps;
+            }
+
+            var common = CommonAncestorOf(nodes["YOU"], nodes["SAN"]);
+            var distUp = DistanceFromTo(nodes["YOU"], common);
+            var distDn = DistanceFromTo(nodes["SAN"], common);
+            Part2 = $"{distUp + distDn}";
         }
     }
 }
