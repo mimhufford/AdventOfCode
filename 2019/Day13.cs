4d902f270321895e5e1782a3bb2df5cdc71131ba
Mim Hufford <mim@hotmail.co.uk>
2019-12-13 07:23:31 +0000

    2019 day 13 optimisation

--- 2019/Day13.cs
+++ 2019/Day13.cs
@@ -14,29 +14,34 @@ namespace AoC
             c.Flash(LongCSV.ToArray());
             c.mem[0] = 2;
             c.Run();
-            var screen = c.outputs.ToArray();
+
             var blocks = 0;
-            for (int i = 2; i < screen.Length; i += 3) if (screen[i] == 2) blocks += 1;
+            while (c.outputs.Count > 0)
+            {
+                c.outputs.Dequeue(); c.outputs.Dequeue();
+                if (c.outputs.Dequeue() == 2) blocks += 1;
+            }
             Part1 = blocks.ToString();
 
             var ballx = 0L;
             var padlx = 0L;
             var score = 0L;
+
             while (!c.done)
             {
                 c.Run(ballx < padlx ? -1 : ballx > padlx ? 1 : 0);
-                screen = c.outputs.ToArray();
 
-                for (int i = 0; i < screen.Length; i += 3)
+                while (c.outputs.Count > 0)
                 {
-                    var x = screen[i];
-                    var y = screen[i + 1];
-                    var o = screen[i + 2];
+                    var x = c.outputs.Dequeue();
+                    var y = c.outputs.Dequeue();
+                    var o = c.outputs.Dequeue();
                     if (x == -1 && y == 0 && score < o) score = o;
                     else if (o == 3) padlx = x;
                     else if (o == 4) ballx = x;
                 }
             }
+
             Part2 = score.ToString();
         }
     }
