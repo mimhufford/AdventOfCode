22ed5d41361c0503a06eb5f3072d285cde86f224
Mim Hufford <mim@hotmail.co.uk>
2019-12-19 21:31:44 +0000

    add quick time print toggle

--- 2019/2019.cs
+++ 2019/2019.cs
@@ -4,25 +4,28 @@
     {
         static void Main()
         {
-            new Day1();
-            new Day2();
-            new Day3();
-            new Day4();
-            new Day5();
-            new Day6();
-            new Day7();
-            new Day8();
-            new Day9();
-            new Day10();
-            new Day11();
-            new Day12();
-            new Day13();
-            new Day14();
-            new Day15();
-            new Day16();
-            new Day17();
-            new Day18();
-            new Day19();
+            //using (new Timed())
+            {
+                new Day1();
+                new Day2();
+                new Day3();
+                new Day4();
+                new Day5();
+                new Day6();
+                new Day7();
+                new Day8();
+                new Day9();
+                new Day10();
+                new Day11();
+                new Day12();
+                new Day13();
+                new Day14();
+                new Day15();
+                new Day16();
+                new Day17();
+                new Day18();
+                new Day19();
+            }
         }
     }
 }
