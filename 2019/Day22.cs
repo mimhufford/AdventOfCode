a80eaa445612f40abe1ec40801b9fafcc9a50454
Mim Hufford <mim@hotmail.co.uk>
2019-12-23 02:47:09 +0000

    fast version of part 1

--- 2019/Day22.cs
+++ 2019/Day22.cs
@@ -10,41 +10,29 @@ namespace AoC
 
         protected override void Solve()
         {
-            // Faster way for part 1: work backwards from
-            // 2019 and track which position it started at
-            // Going to do the slower way first though
-            // cos I think I'll need that for part 2...
+            var card = 2019;
 
-            var deck = Enumerable.Range(0, 10007).ToList();
-
-            foreach (var l in Lines)
+            foreach (string l in Lines)
             {
                 if (l[0] == 'c')
                 {
                     var i = (int.Parse(l.Substring(4)) + 10007) % 10007;
-                    deck.AddRange(deck.Take(i));
-                    deck = deck.Skip(i).ToList();
+                    card -= i;
+                    card += 10007;
+                    card %= 10007;
                 }
                 else if (l[10] == 'i')
                 {
                     var i = int.Parse(l.Substring(20));
-                    var p = 0;
-                    var newDeck = new int[10007];
-                    for (int c = 0; c < 10007; c++)
-                    {
-                        newDeck[p] = deck[c];
-                        p += i;
-                        p %= 10007;
-                    }
-                    deck = newDeck.ToList();
+                    card = card * i % 10007;
                 }
                 else
                 {
-                    deck.Reverse();
+                    card = 10007 - card - 1;
                 }
             }
 
-            Part1 = deck.IndexOf(2019).ToString();
+            Part1 = card.ToString();
         }
     }
 }
