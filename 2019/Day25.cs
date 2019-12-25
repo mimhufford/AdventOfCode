96f07e503fe28e69c031967577c7b6edfc3c6f70
Mim Hufford <mim@hotmail.co.uk>
2019-12-25 08:54:42 +0000

    remove redundant moves

--- 2019/Day25.cs
+++ 2019/Day25.cs
@@ -14,29 +14,24 @@ namespace AoC
             c.Flash(LongCSV.ToArray());
             c.Run();
 
-            var good = new HashSet<string> { "candy cane", "boulder", "food ration", "asterisk", "mutex", "mug", "prime number", "loom" };
-            var bad = new HashSet<string> { "photons", "infinite loop", "giant electromagnet", "escape pod", "molten lava" };
-
             var inputs = new Queue<string>(new List<string>
             {
-                "south", "take boulder", "west", "take asterisk", "east", "east", "take food ration", "west", "north",
-                "east", "take candy cane", "north", "east", "north", "take mug", "south", "west", "north", "take mutex", "north", "take prime number", "south", "south", "south",
-                "east", "north", "take loom", "south", "east", "south", "east", "east",
-                "drop candy cane", "drop loom", "drop food ration", "drop boulder", "north"
-                // @TODO: don't bother picking these 4 up
+                "south", "west", "take asterisk", "east", "north", "east",
+                "north", "east", "north", "take mug", "south", "west", "north",
+                "take mutex", "north", "take prime number", "south", "south",
+                "south", "east", "east", "south", "east", "east", "north"
             });
 
             /*
-              p-#
-              |
-            #-m m
-              | |
-              #-# l  
-              |   |  
-            O-c---#-#   D
-            |       |   |
-          a-b-f   #-#-#-#
-
+                          p-#
+                          |
+                        #-m m
+                          | |
+                          #-# l  
+                          |   |  
+                        O-c---#-#   D
+                        |       |   |
+                      a-b-f   #-#-#-#
             */
 
             do
