fc03e5b402303038430b8eccc28a18d852d50fe8
mim <mim@hotmail.co.uk>
2022-01-06 13:24:49 +0000

    day 20

--- 2016/20.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const ranges = require('./data').day20.trim().split("\n").map(line => line.match(/(\d+)-(\d+)/).slice(1).map(Number))
-
-const findLowest = i => (ranges.filter(range => range[0] <= i && range[1] >= i).length == 0) ? i : findLowest(i + 1)
-
-console.log(findLowest(0))
